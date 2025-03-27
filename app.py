2e7a03147750a71cf46c788a560bf9d9231f4bad
RoxyHuang0813 <roxyhuang@gmail.com>
2025-03-27 23:17:25 +0800

    Added gunicorn

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def serve_static(filename):
     return send_from_directory('static', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
